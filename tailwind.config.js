7a94e2427e46ec402545aef7262df9a00aa0b7d0
Deon Roos <44850848+DotDeon@users.noreply.github.com>
2022-01-12 17:54:48 +0200

    1

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      black: "#000000",
+      white: "#FFFFFF",
+      purple: "#b6b7ff",
+      green: "#b4ffc7",
+      yellow: "#ffeea6",
+      pink: "#ffd0e6",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
