997e88f190aa2b063258790c20f37c570d5b810c
Deon Roos <44850848+DotDeon@users.noreply.github.com>
2022-01-19 00:34:10 +0200

    Contract Updated

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -45,10 +45,10 @@ export const connect = () => {
         });
         //const NetworkData = await SmartContract.networks[networkId];
         // change network ID to 1
-        if (networkId == 4) {
+        if (networkId == 1) {
           const SmartContractObj = new web3.eth.Contract(
             SmartContract.abi,
-            '0x002B8f409723e2A762a8EC86552bc336CCd17948'
+            '0xf51e3D4c0dFBc440De5Ab34d3B658E342DF26388'
           );
           dispatch(
             connectSuccess({
